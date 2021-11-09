b339843cc48bd925f5a86557a2703366a7669a4f
dev-from-cbd <veryfatwombat@gmail.com>
2021-11-10 09:10:45 +1100

    react js

---
+++ todo/src/App.js
@@ -0,0 +1,19 @@
+import logo from "./to-do-list.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img
+          src={logo}
+          className="App-logo"
+          alt="ToDo List App with React JS using LocalStorage"
+        />
+        <p>ToDo List App with React JS using LocalStorage</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
