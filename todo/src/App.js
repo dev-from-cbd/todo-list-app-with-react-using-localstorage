8bac558bb48e5d67241e64418207755177e4afb9
dev-from-cbd <veryfatwombat@gmail.com>
2021-11-13 08:07:36 +1100

    react

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Layout from "./components/Layout";
 
 const App = () => {
   return (
-    <div>
-      <h1>Todo List App with React JS using LocalStorage</h1>
-    </div>
+    <Layout>
+      <h1>Hello!</h1>
+    </Layout>
   );
 };
 
--- todo/src/components/Layout.js
+++ todo/src/components/Layout.js
@@ -3,7 +3,7 @@ import React from "react";
 const Layout = ({ children }) => {
   return (
     <div className="container p-3 p-sm-1">
-      <div className="row">
+      <div className="row justify-content-center">
         <div className="col-xl-6 col-lg-7 col-md-10">
           <div className="card">
             <div className="card-body">{children}</div>
