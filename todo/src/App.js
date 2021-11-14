2b0238d1519f90179ad1f204f6446c7d52684aaf
dev-from-cbd <veryfatwombat@gmail.com>
2021-11-14 20:11:29 +1100

    react js

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,10 +1,70 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Form from "./components/Form";
+import Header from "./components/Header";
 import Layout from "./components/Layout";
+import Lists from "./components/Lists";
 
 const App = () => {
+  const [error, setError] = useState(null);
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem("todos"));
+    if (storedTodos) setTodos(storedTodos);
+  }, []);
+
+  // saving the todos in browser storage to prevent loss of todos on refreshing tab
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    if (todo.length < 5) {
+      setError("At least 5 word required!");
+      return false;
+    }
+
+    setTodos([{ id: Date.now(), title: todo, done: false }, ...todos]);
+
+    setTodo("");
+    setError(null);
+  };
+
+  const delHandler = (todoId) => {
+    if (window.confirm("Are you sure")) {
+      const updatedTodos = todos.filter((item) => item.id !== todoId);
+
+      setTodos(updatedTodos);
+    }
+  };
+
+  const doneHandler = (todoId) => {
+    const index = todos.findIndex((emp) => emp.id === todoId);
+    const newTodo = [...todos];
+
+    newTodo[index] = {
+      id: todos[index].id,
+      title: todos[index].title,
+      done: !todos[index].done,
+    };
+
+    setTodos(newTodo);
+  };
+
   return (
     <Layout>
-      <h1>Hello!</h1>
+      <Header />
+      <Form
+        error={error}
+        value={todo}
+        submit={submitHandler}
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      <hr className="border-primary" />
+      <Lists todos={todos} delHandler={delHandler} doneHandler={doneHandler} />
     </Layout>
   );
 };
